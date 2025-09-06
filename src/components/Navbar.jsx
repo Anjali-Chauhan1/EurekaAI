0bc14d2badb1e9c0616259680b41dfd821e4ac1f
Anjali-Chauhan1 <anjalichauhan1036@gmail.com>
2025-09-07 05:21:32 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,17 +7,14 @@ export default function Navbar() {
     <nav className="fixed top-0 left-0 w-full z-50 
       bg-gray-900/70 backdrop-blur-md border-b border-white/10 
       px-6 py-4 shadow-lg flex justify-between items-center">
-      
-      <Link 
+       <Link 
         to="/" 
         className="text-2xl font-bold text-white hover:text-transparent 
         bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500 transition"
       >
         EurekaAI
       </Link>
-
-
-      <div className="space-x-4 text-lg font-medium flex">
+       <div className="space-x-4 text-lg font-medium flex">
         <Link 
           to="/" 
           className="px-4 py-2 rounded-lg text-gray-200 hover:text-white 
@@ -26,8 +23,7 @@ export default function Navbar() {
         >
           Home
         </Link>
-
-        <Link 
+         <Link 
           to="/about" 
           className="px-4 py-2 rounded-lg text-gray-200 hover:text-white 
           hover:bg-gradient-to-r hover:from-blue-500 hover:to-purple-600 
