4e3f9ec3cbbd2cabc75ae6f7725ce685828b1b30
Anjali-Chauhan1 <anjalichauhan1036@gmail.com>
2025-09-07 05:22:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,20 @@ import TopicPage from './pages/TopicPage';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
-
-
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-        
-        <Navbar />
-
-        <main className="flex-grow">
+         <Navbar />
+         <main className="flex-grow">
           <Routes>
-            
-            <Route path="/" element={<Home />} />
+             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/topic/:topicName" element={<TopicPage />} />
             <Route path="*" element={<div className="text-center mt-20 text-2xl">404 - Page Not Found</div>} />
           </Routes>
         </main>
-
-        <Footer />
+         <Footer />
       </div>
     </Router>
   );
